6363703118a9ed694b2e4877a17ccd7f6a25b24c
wardaturrahmah <45145286+wardaturrahmah@users.noreply.github.com>
2018-11-18 22:06:16 +0700

    Update array_of_struct.c

--- array_of_struct.c
+++ array_of_struct.c
@@ -1,12 +1,9 @@
 #include<iostream>
 using namespace std;
+//deklarasi struct matkul, dengan array 10
 struct matkul{
 	string nama;
 	int sks;
-	int uas;
-	int uts;
-	int nas;
-
 } matkul[10];
 
 main()
@@ -14,5 +11,5 @@ main()
 
 matkul[0].nama="wardah"; //mengisi array of struct matkul indeks ke 0
 matkul[0].sks=3;
-std::cout << matkul[0].nama << '\n'; //menampilkan array of struct matkul indeks ke 0
+std::cout << matkul[0].nama << '\n'; //menampilkan variabel nama pada array of struct matkul indeks ke 0
 }
